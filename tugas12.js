903cf4065fcba69e609acb441be81095ce6d3380
hugoiman <hugogiman17@gmail.com>
2020-07-04 12:58:36 +0700

    Tugas 12 Javascript

---
+++ tugas12.js
@@ -0,0 +1,12 @@
+function tugas12() {
+  var buah = ["Pisang", "Jeruk", "Apel", "Mangga"];
+  console.log(buah);
+
+  var buah2 = buah.pop();
+  console.log(buah);
+
+  var buah3 = buah.shift();
+  console.log(buah);
+}
+
+tugas12();
